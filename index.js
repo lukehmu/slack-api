f563ed1b560b734e1c53840c3de1bd13035c3616
Luke <luke@electricputty.co.uk>
2019-07-11 15:46:45 +0100

    Attemp to send status!

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
     let text = req.body.text
-    var data = {form: {
-    token: process.env.SLACK_AUTH_TOKEN,
-    "profile": {
+    let data =
+    {
+        "profile": {
         "status_text": "riding a train",
         "status_emoji": ":mountain_railway:",
         "status_expiration": 0
     }
-    }};
+}
 request.post('https://slack.com/api/users.profile.set', data, function (error, response, body) {
       // Sends welcome message
       //console.log(error)
@@ -32,6 +32,22 @@ request.post('https://slack.com/api/users.profile.set', data, function (error, r
       res.json()
     });
 });
+
+// app.post('/', (req, res) => {
+//     let text = req.body.text
+//     var data = {form: {
+//     token: process.env.SLACK_AUTH_TOKEN,
+//     channel: "#luke-test",
+//     text: processText(text)
+//     }};
+// request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+//       // Sends welcome message
+//       //console.log(error)
+//       console.log(body)
+//       //console.log(body)
+//       res.json()
+//     });
+// });
 
 function processText(text) {
     return(text)
